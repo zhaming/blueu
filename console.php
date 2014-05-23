4d00b1ff19cd947752016710888ffff827871fbd
zha_ming <zha_ming@163.com>
2014-05-23 18:15:00 +0800

    optimize the configuration

---
+++ console.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *	控制台入口
+ *	@author		zham <zha_ming@163.com>
+ *	@copyright	2014-2016
+ *	@version	1.0
+ *	@package	
+ *
+ *	$Id$
+ */
+
+$config = dirname(__FILE__).'/protected/config/console.php';
+require_once dirname(__FILE__).'/framework/yiic.php';
--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
-
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+/**
+ *	Web端入口
+ *	@author		zham <zha_ming@163.com>
+ *	@copyright	2014-2016
+ *	@version	1.0
+ *	@package	
+ *
+ *	$Id$
+ */
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',4);
 
-require_once($yii);
+require_once dirname(__FILE__).'/framework/yii.php';
+$config = dirname(__FILE__).'/protected/config/main.php';
 Yii::createWebApplication($config)->run();
