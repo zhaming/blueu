a0a11a8d0f1e6e3622124d65a7f6baec989ee969
zha_ming <zha_ming@163.com>
2014-05-23 14:29:23 +0800

    optimize the configuration

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,17 +1,42 @@
 <?php
-return array(
-    'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'My Console Application',
-    'components'=>array(
-        'db'=>array(
-            'connectionString' => 'mysql:host=localhost;dbname=blueu',
-            'emulatePrepare'   => true,
-            'username'         => 'root',
-            'password'         => 'admin',
-            'charset'          => 'utf8',
-            'tablePrefix'      => ''
-        ),
-    ),
-);
-     
-?>
+/**
+ *	控制台配置入口
+ *	@author		zham <zha_ming@163.com>
+ *	@copyright	2014-2016
+ *	@version	1.0
+ *	@package	MING
+ *
+ *	$Id$
+ */
+
+return array_merge_recursive(array(
+	'basePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+    'name' => 'Ming',
+    
+	'import' => array(
+		'application.models.*',
+		'application.components.*',
+		'application.behaviors.*',
+		'application.extensions.*',
+        'application.extensions.baidupush.*',
+	),
+
+	'components' => array(
+		'log' => array(
+			'class' => 'CLogRouter',
+			'routes' => array(
+				array(
+					'class' => 'CFileLogRoute',
+					'levels' => 'error,warning,trace',
+				),
+				array(
+					'class' => 'CWebLogRoute',
+                    'levels' => 'error,warning',
+					'showInFireBug' => true,
+					'ignoreAjaxInFireBug' => true,
+				),
+			),
+		),
+	),
+	'params'=>require('params.php'),
+), require('db.php'));
