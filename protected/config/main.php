84f22daab963e15951abdee6f35320096d749c17
zha_ming <zha_ming@163.com>
2014-05-23 12:55:08 +0800

    ignore the configuration

--- protected/config/main.php
+++ protected/config/main.php
@@ -74,10 +74,10 @@ return array(
          */
         // uncomment the following to use a MySQL database
         'db' => array(
-            'connectionString' => 'mysql:host=localhost;dbname=blueu',
+            'connectionString' => 'mysql:host=127.0.0.1;dbname=blueu2',
             'emulatePrepare' => true,
             'username' => 'root',
-            'password' => 'admin',
+            'password' => 'mysql',
             'charset' => 'utf8',
             'tablePrefix' => '',
         ),
