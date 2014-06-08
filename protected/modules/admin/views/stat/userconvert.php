536b3fc0ac32d7e9e8d93b0b862466d634416f5f
zha_ming <zha_ming@163.com>
2014-06-08 12:05:57 +0800

    update style

--- protected/modules/admin/views/stat/userconvert.php
+++ protected/modules/admin/views/stat/userconvert.php
@@ -4,17 +4,17 @@
             <ul class="nav nav-tabs padding-16">
                 <li>
                     <a href="<?php echo $this->createUrl('user?t=info'); ?>">
-                        <i class="green icon-sun bigger-125"></i><?php echo Yii::t('admin', 'VStatUserInfo'); ?>
+                        <i class="green ace-icon fa fa-sun-o bigger-125"></i><?php echo Yii::t('admin', 'VStatUserInfo'); ?>
                     </a>
                 </li>
                 <li class="active">
                     <a href="<?php echo $this->createUrl('user?t=convert'); ?>">
-                        <i class="green icon-edit bigger-125"></i><?php echo Yii::t('admin', 'VStatUserConvert'); ?>
+                        <i class="green ace-icon fa fa-edit bigger-125"></i><?php echo Yii::t('admin', 'VStatUserConvert'); ?>
                     </a>
                 </li>
                 <li>
                     <a href="<?php echo $this->createUrl('user?t=share'); ?>">
-                        <i class="green icon-edit bigger-125"></i><?php echo Yii::t('admin', 'VStatUserShare'); ?>
+                        <i class="green ace-icon fa fa-edit bigger-125"></i><?php echo Yii::t('admin', 'VStatUserShare'); ?>
                     </a>
                 </li>
             </ul>
